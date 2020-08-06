5191ec1f431edad1763e48787851921cb40468db
Divinewill <51236443+divinoeze@users.noreply.github.com>
2020-08-06 10:12:22 +0100

    Added JWT for laravel API Authentication

--- resources/js/app.js
+++ resources/js/app.js
@@ -77,3 +77,19 @@ const app = new Vue({
     el: "#app",
     router
 });
+
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
